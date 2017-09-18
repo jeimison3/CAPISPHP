cc98833a9879caa56bde4711e567c3864c2ac825
jeimison3 <jeimison3@gmail.com>
2017-09-17 23:43:55 -0300

    Bug de string ausente corrigido.

--- mod_FileUploads.php
+++ mod_FileUploads.php
@@ -21,7 +21,7 @@ class FileUpload{
     $extensao_arquivo = strtolower(pathinfo($uploadFile["name"],PATHINFO_EXTENSION)); //Extrai a extensão do arquivo em caixa baixa
     $nome_trajeto=basename($uploadFile["name"]); //Usa o nome original
     if($this->regrasVector['renbymd5_fname']){ //Se regra verdadeira, renomeia usando MD5
-      $nome_trajeto=md5(basename($uploadFile["name"]).date().rand()).".".$extensao_arquivo;
+      $nome_trajeto=md5(basename($uploadFile["name"]).date("Y-m-d H:i:s").rand()).".".$extensao_arquivo;
     }
     $nome_trajeto=$this->regrasVector['prefix_name'].$nome_trajeto; //Insere prefixo no nome
     $arquivo_trajeto = $this->regrasVector['path_name'] . $nome_trajeto; //Cria variável com pasta e nome final
